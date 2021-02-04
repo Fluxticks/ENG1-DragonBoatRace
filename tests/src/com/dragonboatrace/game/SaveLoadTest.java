2b817c67a46eb603557c229fbaed44bbe3cad04e
Fluxticks <benjigarment.appdev@gmail.com>
2021-02-04 15:11:33 +0000

    Added framework for Save/Loading testing

---
+++ tests/src/com/dragonboatrace/game/SaveLoadTest.java
@@ -0,0 +1,117 @@
+package com.dragonboatrace.game;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.files.FileHandle;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.utils.JsonReader;
+import com.badlogic.gdx.utils.JsonValue;
+import com.dragonboatrace.game.entities.*;
+import com.dragonboatrace.game.screens.GameScreen;
+import org.junit.*;
+import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
+
+import java.io.File;
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+@RunWith(GdxTestRunner.class)
+public class SaveLoadTest {
+
+    /*@BeforeClass
+    public static void clearTestSaves(){
+        System.out.println("Clearing Directory");
+        File directory = new File("TestSaves");
+        File[] files = directory.listFiles();
+        for(File f : files){
+            f.delete();
+        }
+    }*/
+
+    @Test
+    public void _0_saveBoatTest(){
+        PlayerBoat boat = new PlayerBoat(BoatType.TESTING,new Vector2(), new Tuple<Float, Float>(0f, 500f));
+        String output = boat.save();
+        String knownCorrect = "{type:TESTING, health:300.000000, stamina:1000.000000, distance:0.000000, totalTime:0, " +
+                                "laneBounds:{x:0.000000, y:500.000000}, inGamePos:{x:0.000000, y:0.000000}, " +
+                                "pos:{x:0.000000, y:0.000000}, vel:{x:0.000000, y:0.000000}}";
+        Assert.assertEquals(knownCorrect, output);
+        Assert.assertTrue(GameScreen.saveJSONString(output, Gdx.files.local("TestingSaves/boatSaveTest.json")));
+        System.out.println("Done Save Boat Test");
+    }
+
+    @Test
+    public void _0_saveObstacleTest(){
+        Obstacle obstacle = new Obstacle(ObstacleType.TESTING,new Vector2(), new Vector2());
+        String output = obstacle.save();
+        String knownCorrect = "{type:TESTING, pos:{x:0.000000, y:0.000000}, vel:{x:0.000000, y:0.000000}, " +
+                                "constantVel:{x:0.000000, y:0.000000}}";
+        Assert.assertEquals(knownCorrect, output);
+        Assert.assertTrue(GameScreen.saveJSONString(output, Gdx.files.local("TestingSaves/obstacleSaveTest.json")));
+        System.out.println("Done Save Obstacle Test");
+    }
+
+    @Test
+    public void _0_savePowerUpTest(){
+        PowerUp power = new PowerUp(PowerUpType.TESTING, new Vector2(), new Vector2());
+        String output = power.save();
+        String knownCorrect = "{type:TESTING, pos:{x:0.000000, y:0.000000}, vel:{x:0.000000, y:0.000000}, " +
+                                "constantVel:{x:0.000000, y:0.000000}}";
+        Assert.assertEquals(knownCorrect, output);
+        Assert.assertTrue(GameScreen.saveJSONString(output, Gdx.files.local("TestingSaves/powerUpSaveTest.json")));
+        System.out.println("Done Save Power Up Test");
+    }
+
+    @Test
+    public void _0_saveLaneTest(){
+        PlayerBoat boat = new PlayerBoat(BoatType.TESTING,new Vector2(), new Tuple<Float, Float>(0f, 500f));
+        Lane lane = new Lane(boat, boat);
+        lane.updateRound(1, 0.5f);
+        String output = lane.save();
+        String knownCorrect = "{boat:{type:TESTING, health:300.000000, stamina:1000.000000, distance:0.000000, " +
+                                "totalTime:0, laneBounds:{x:0.000000, y:500.000000}, inGamePos:{x:0.000000, y:0.000000}, " +
+                                "pos:{x:0.000000, y:0.000000}, vel:{x:0.000000, y:0.000000}}, obstacles:[], " +
+                                "powerup:null, isPlayer:1}";
+        Assert.assertEquals(knownCorrect, output);
+        Assert.assertTrue(GameScreen.saveJSONString(output, Gdx.files.local("TestingSaves/laneSaveTest.json")));
+        System.out.println("Done Save Lane Test");
+    }
+
+    @Test
+    public void _1_loadBoatTest(){
+        PlayerBoat boatKnown = new PlayerBoat(BoatType.TESTING,new Vector2(), new Tuple<Float, Float>(0f, 500f));
+        FileHandle file = Gdx.files.local("TestingSaves/boatSaveTest.json");
+        JsonValue jsonString = new JsonReader().parse(file);
+        PlayerBoat boatOutput = new PlayerBoat(jsonString);
+        // Check Type is loaded correctly
+        Assert.assertEquals(boatKnown.getBoatType(), boatOutput.getBoatType());
+        // Check Health is loaded correctly
+        Assert.assertEquals(boatKnown.getHealth(), boatOutput.getHealth(), 0.0);
+        // Check Stamina is loaded correctly
+        Assert.assertEquals(boatKnown.getStamina(), boatOutput.getStamina(), 0.0);
+        // Check distance travelled is loaded correctly
+        Assert.assertEquals(boatKnown.getDistanceTravelled(), boatOutput.getDistanceTravelled(), 0.0);
+        // Check total time is loaded correctly
+        Assert.assertEquals(boatKnown.getTotalTimeLong(), boatOutput.getTotalTimeLong());
+        // Check lane bounds is loaded correctly
+        Assert.assertEquals(boatKnown.getLaneBounds(), boatOutput.getLaneBounds());
+        // Check In-Game pos is loaded correctly
+        Assert.assertEquals(boatKnown.getInGamePos(), boatOutput.getInGamePos());
+        // Check Pos is loaded correctly
+        Assert.assertEquals(boatKnown.getPos(), boatOutput.getPos());
+        // Check Vel is loaded correctly
+        Assert.assertEquals(boatKnown.getVel(), boatOutput.getVel());
+    }
+
+    public void _1_loadObstacleTest(){
+
+    }
+
+    public void _1_loadPowerUpTest(){
+
+    }
+
+    public void _1_loadLaneTest(){
+
+    }
+
+}
