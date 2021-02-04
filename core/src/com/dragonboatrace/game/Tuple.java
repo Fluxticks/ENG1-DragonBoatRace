404e6d5f5a77c0002e11748f2af03a19f220ad05
Fluxticks <benjigarment.appdev@gmail.com>
2021-02-04 15:09:27 +0000

    Added equals() to Tuple to enable equality between tuple values.

--- core/src/com/dragonboatrace/game/Tuple.java
+++ core/src/com/dragonboatrace/game/Tuple.java
@@ -9,6 +9,16 @@ public class Tuple<A, B>{
         this.b = b;
     }
 
+    @Override
+    public boolean equals(Object obj){
+        if(obj.getClass() == Tuple.class){
+            Tuple objT = (Tuple)obj;
+            return this.a.equals(objT.a) && this.b.equals(objT.b);
+        }else{
+            return false;
+        }
+    }
+
     public String toString() {
         return String.format("Tuple<%s, %s>", this.a.toString(), this.b.toString());
     }
