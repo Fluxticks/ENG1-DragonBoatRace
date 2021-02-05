347223a36a4d62c6a768078c476f73fbab742636
Fluxticks <benjigarment.appdev@gmail.com>
2021-02-05 16:15:10 +0000

    Lane javadoc done

--- core/src/com/dragonboatrace/game/Lane.java
+++ core/src/com/dragonboatrace/game/Lane.java
@@ -13,34 +13,65 @@ import java.util.Arrays;
 import java.util.ListIterator;
 import java.util.concurrent.ThreadLocalRandom;
 
+/**
+ * Represents a lane to be used for separating boats and obstacles into specific lanes.
+ *
+ * @author Benji Garment
+ */
 public class Lane {
 
+    /**
+     * The list of the current obstacles in the lane.
+     */
     private ArrayList<Obstacle> obstacles;
-
+    /**
+     * The currently available power up in the lane.
+     */
     private PowerUp currentPower;
-
+    /**
+     * The chance per-frame for a power up to be spawned.
+     */
     private final float powerChance = 0.05f;
-
+    /**
+     * The current round of the race.
+     */
     private int round;
-
+    /**
+     * The boat in the lane.
+     */
     private final Boat boat;
-
+    /**
+     * The maximum number of obstacles in the lane.
+     */
     private int maxObstacles;
-
+    /**
+     * An instance of the player boat.
+     */
     private PlayerBoat pb;
-
+    /**
+     * A boolean of if the lane belongs to the player.
+     */
     public boolean isPlayerLane;
 
-
+    /**
+     * Creates a new lane given a boat and an instance of the player boat.
+     *
+     * @param boatInLane The boat that is in the lane.
+     * @param pb         An instance of the player boats lane.
+     */
     public Lane(Boat boatInLane, PlayerBoat pb) {
         this.boat = boatInLane;
         this.obstacles = new ArrayList<>();
         this.currentPower = null;
         this.pb = pb;
-        //this.isPlayerLane = (this.pb == this.boat);
         this.isPlayerLane = boatInLane instanceof PlayerBoat;
     }
 
+    /**
+     * Creates a new lane given a json string loaded from a save file.
+     *
+     * @param jsonString A json string representing a lane.
+     */
     public Lane(JsonValue jsonString) {
         if (jsonString.getInt("isPlayer") == 1) {
             this.boat = new PlayerBoat(jsonString.get("boat"));
@@ -53,7 +84,6 @@ public class Lane {
             this.obstacles.add(new Obstacle(o));
         }
 
-
         this.isPlayerLane = jsonString.getInt("isPlayer") == 1;
         if (jsonString.get("powerup").get(0) != null) {
             this.currentPower = new PowerUp(jsonString.get("powerup"));
@@ -61,9 +91,14 @@ public class Lane {
             this.currentPower = null;
         }
         this.pb = null;
-
     }
 
+    /**
+     * Compares an object to a lane and determines if the object is an identical lane.
+     *
+     * @param obj The object being checked against.
+     * @return A boolean of weather the object is an identical lane.
+     */
     @Override
     public boolean equals(Object obj) {
         if (obj == null) {
@@ -92,10 +127,20 @@ public class Lane {
         }
     }
 
+    /**
+     * Get the currently available power up.
+     *
+     * @return A power up or null if no power up available.
+     */
     public PowerUp getCurrentPower() {
         return currentPower;
     }
 
+    /**
+     * Get a json string of the boat, power up and obstacles in the lane.
+     *
+     * @return A string representing the lane and all the attributes in the lane.
+     */
     public String save() {
         String[] obstacleStrings = new String[this.obstacles.size()];
         for (int i = 0; i < obstacleStrings.length; i++) {
@@ -111,6 +156,16 @@ public class Lane {
 
     }
 
+    /**
+     * Update the contents of the lane.
+     *
+     * @param deltaTime The time since the previous frame.
+     * <p>
+     * @see Boat
+     * @see Obstacle
+     * @see PowerUp
+     * </p>
+     */
     public void update(float deltaTime) {
         ListIterator<Obstacle> iter = obstacles.listIterator();
         while (iter.hasNext()) {
@@ -146,6 +201,16 @@ public class Lane {
         this.boat.update(deltaTime);
     }
 
+    /**
+     * Add the contents to the given SpriteBatch and render it.
+     *
+     * @param batch The spritebatch to be added to.
+     * <p>
+     * @see Boat
+     * @see Obstacle
+     * @see PowerUp
+     * </p>
+     */
     public void render(SpriteBatch batch) {
         for (Obstacle obstacle : this.obstacles) {
             obstacle.render(batch, this.pb.getInGamePos());
@@ -157,16 +222,31 @@ public class Lane {
         //renderBounds();
     }
 
+    /**
+     * Check if a given entity is still on the screen.
+     *
+     * @param entity The entity being checked.
+     * @return A boolean of if the given entity is on the screen.
+     */
     private boolean checkEntityNotOnScreen(Entity entity) {
         Vector2 renderPos = entity.getRelPos(this.pb.getInGamePos());
         return renderPos.x > Gdx.graphics.getWidth() + 30 || renderPos.x + entity.getSize().x < -30 || renderPos.y + entity.getSize().y < -100;
-        //return renderPos.x > this.boat.getLaneBounds().b + 30 || renderPos.x + entity.getSize().x < this.boat.getLaneBounds().a - 30 || renderPos.y + entity.getSize().y < -100;
     }
 
+    /**
+     * Check if a power up is in the lane bounds.
+     *
+     * @return A boolean of if the power up is in the lane.
+     */
     private boolean checkPowerUpNotInEdges() {
         return this.currentPower.getInGamePos().x < this.boat.getLaneBounds().a + 5 || this.currentPower.getInGamePos().x + this.currentPower.getSize().x > this.boat.getLaneBounds().b - 5;
     }
 
+    /**
+     * Perform all the updates required for the current power up.
+     *
+     * @param deltaTime The time since the previous frame.
+     */
     private void updatePowerUp(float deltaTime) {
         if (this.boat.getHitbox().checkCollision(this.currentPower.getHitbox())) {
             this.currentPower.applyEffect(this.boat);
@@ -181,7 +261,11 @@ public class Lane {
         }
     }
 
-
+    /**
+     * Render the edges of the lane.
+     *
+     * @param shapeRenderer The renderer used to display the lines.
+     */
     private void renderBounds(ShapeRenderer shapeRenderer) {
         Tuple<Float, Float> bounds = this.boat.getLaneBounds();
         shapeRenderer.begin(ShapeRenderer.ShapeType.Line);
@@ -191,6 +275,13 @@ public class Lane {
 
     }
 
+    /**
+     * Update the current round.
+     *
+     * @param newRound           The round to be set to.
+     * @param obstacleMultiplier The multiplier for the number of obstacles.
+     * @return The new number of max obstacles.
+     */
     public int updateRound(int newRound, float obstacleMultiplier) {
         this.round = newRound;
 
@@ -198,31 +289,12 @@ public class Lane {
 
         this.maxObstacles = (int) ((offset + newRound) * obstacleMultiplier);
         return this.maxObstacles;
-
-        /*
-        switch (this.round) {    // The max number of obstacles changes from round to round
-            case 0:
-                this.maxObstacles = (int)(3 * obstacleMultiplier);
-                break;
-            case 1:
-                this.maxObstacles = (int)(4 * obstacleMultiplier);
-                break;
-            case 2:
-                this.maxObstacles = (int)(5 * obstacleMultiplier);
-                break;
-            case 3:
-                this.maxObstacles = (int)(6 * obstacleMultiplier);
-                break;
-            default:
-                this.maxObstacles = 0;
-                break;
-        }*/
     }
 
-    public void updateMaxObstacles(int newObstacleCount) {
-        this.maxObstacles = newObstacleCount;
-    }
-
+    /**
+     * Create a new random obstacle at a random position in the lane.
+     * @return An obstacle of random type and at random position.
+     */
     private Obstacle spawnObstacle() {
         Vector2 spawnPos;    // position the obstacle will spawn at
         Vector2 dir;        // direction the obstacle will start travelling in
@@ -268,6 +340,10 @@ public class Lane {
         return new Obstacle(obs, spawnPos, dir);    // Return the new obstacle
     }
 
+    /**
+     * Randomly create a new power up. Return null if it doesn't spawn one.
+     * @return Null if no obstacle spawned, otherwise an obstacle of random type and in a random place in the lane.
+     */
     private PowerUp spawnPowerUp() {
 
         float random = ThreadLocalRandom.current().nextFloat();
@@ -296,6 +372,11 @@ public class Lane {
         }
     }
 
+    /**
+     * Choose a random location on a side to spawn an obstacle.
+     * @param side The side to choose from.
+     * @return A vector2d of the position to spawn the obstacle on a side.
+     */
     private Vector2 createObstacleOnSide(int side) {
         Vector2 spawnPos;
         if (side == 0) {
@@ -315,40 +396,79 @@ public class Lane {
         return spawnPos;
     }
 
+    /**
+     * Move the boat to the start and reset the obstacles and power up.
+     */
     public void moveBoatToStart() {
         this.boat.moveToStart();
         this.currentPower = null;
         this.obstacles = new ArrayList<>();
     }
 
-    public boolean checkBoatFinished(int finishTime, long startTime) {
-        return this.boat.checkFinished(finishTime, startTime);
+    /**
+     * Check if the boat has crossed the finish line.
+     * @param finishLine The position of the finish line.
+     * @param startTime The time at which the boat started the round.
+     * @return A boolean of if the boat has crossed the line.
+     */
+    public boolean checkBoatFinished(int finishLine, long startTime) {
+        return this.boat.checkFinished(finishLine, startTime);
     }
 
+    /**
+     * Set the boats finish time.
+     * @param finishTime The time to set the finish time to.
+     */
     public void setBoatFinishTime(long finishTime) {
         this.boat.setFinishTime(finishTime);
     }
 
+    /**
+     * Get the boat that is in the lane.
+     * @return A boat of type {@link PlayerBoat} or {@link CPUBoat}
+     */
     public Boat getBoat() {
         return this.boat;
     }
 
+    /**
+     * Set the instance of the player boat.
+     * @param pb The boat to set as the player boat.
+     */
     public void setPb(PlayerBoat pb) {
         this.pb = pb;
     }
 
+    /**
+     * Get the boat in the lane's render position.
+     * @return A vector2d of the boat's in-game position.
+     */
     public Vector2 getBoatGamePos() {
         return this.boat.getInGamePos();
     }
 
+    /**
+     * Get the time at which the boat finished as a long.
+     * @return A long of the time at which the boat finished.
+     */
     public long getBoatFinishTimeLong() {
         return this.boat.getFinishTimeLong();
     }
 
+    /**
+     * Get the total time of all the rounds added together the boat has done.
+     * @return A long of all the rounds added up the boat has done.
+     */
     public long getBoatTotalTimeLong() {
         return this.boat.getTotalTimeLong();
     }
 
+    /**
+     * Dispose of the boat texture, current power up texture and the obstacle textures.
+     * @see Boat
+     * @see PowerUp
+     * @see Obstacle
+     */
     public void dispose() {
         for (Obstacle obstacle : obstacles) {
             obstacle.dispose();
@@ -356,6 +476,9 @@ public class Lane {
         this.boat.dispose();
     }
 
+    /**
+     * Load the boats texture.
+     */
     public void loadTexture() {
         this.boat.loadTexture();
     }
