2cf424891ae59459c424c62d8fa4316038471633
Fluxticks <benjigarment.appdev@gmail.com>
2021-02-05 15:13:15 +0000

    PowerUp javadoc done

--- core/src/com/dragonboatrace/game/entities/PowerUp.java
+++ core/src/com/dragonboatrace/game/entities/PowerUp.java
@@ -7,12 +7,33 @@ import com.badlogic.gdx.utils.Json;
 import com.badlogic.gdx.utils.JsonValue;
 import com.badlogic.gdx.utils.Timer;
 
+/**
+ * Represents a Power Up obstacle
+ *
+ * @author Benji Garment
+ */
 public class PowerUp extends Entity {
 
+    /**
+     * The type of Power Up.
+     */
     protected PowerUpType type;
+    /**
+     * The constant velocity, used to reset the current velocity.
+     */
     protected Vector2 constantVel;
+    /**
+     * The texture of the power up.
+     */
     protected Texture image;
 
+    /**
+     * Creates a new power up given a power up type as a template, an initial position and an initial velocity.
+     *
+     * @param type     The power up type used as a template.
+     * @param pos      The initial position of the power up.
+     * @param velocity The initial velocity of the power up.
+     */
     public PowerUp(PowerUpType type, Vector2 pos, Vector2 velocity) {
         super(pos, type.getSize(), type.getWeight());
         this.type = type;
@@ -20,6 +41,11 @@ public class PowerUp extends Entity {
         this.vel = velocity;
     }
 
+    /**
+     * Creates a new power up from a json string loaded from a save file.
+     *
+     * @param jsonString The json string representing the power up.
+     */
     public PowerUp(JsonValue jsonString) {
         super(new Vector2(jsonString.get("pos").getFloat("x"), jsonString.get("pos").getFloat("y")),
                 new Json().fromJson(PowerUpType.class, jsonString.getString("type")).getSize(),
@@ -29,11 +55,19 @@ public class PowerUp extends Entity {
         this.type = new Json().fromJson(PowerUpType.class, jsonString.getString("type"));
     }
 
+    /**
+     * Load the texture from the image source. Is required before rendering.
+     */
     @Override
     public void loadTexture() {
         this.image = new Texture(this.type.imageSrc);
     }
 
+    /**
+     * Creates a json string of all the current attributes of the power up.
+     *
+     * @return A json string representing a power up.
+     */
     public String save() {
         return String.format("{type:%s, pos:{x:%f, y:%f}, vel:{x:%f, y:%f}, constantVel:{x:%f, y:%f}}",
                 this.type,
@@ -46,7 +80,13 @@ public class PowerUp extends Entity {
         );
     }
 
-    public void applyEffect(Boat boatAffected) throws NullPointerException{
+    /**
+     * Apply the effect of the power up to the boat given.
+     *
+     * @param boatAffected The boat to be affected by the effect.
+     * @throws NullPointerException If the power up type is not a valid type, a NullPointerException will be thrown.
+     */
+    public void applyEffect(Boat boatAffected) throws NullPointerException {
         switch (this.type) {
             case SPEED:
                 this.applySpeed(boatAffected);
@@ -72,41 +112,77 @@ public class PowerUp extends Entity {
     }
 
     /*
-    * Refactored methods to so they can be tested
-    */
+     * Refactored methods to so they can be tested
+     */
 
-    public float applySpeed(Boat boatAffected){
+    /**
+     * Apply the speed effect.
+     *
+     * @param boatAffected The boat being affected by the power up.
+     * @return A float of the delta between the speed before and after the power up.
+     */
+    public float applySpeed(Boat boatAffected) {
         float velBefore = boatAffected.vel.y;
         boatAffected.increaseYVelocity(type.effect);
         return boatAffected.vel.y - velBefore;
     }
 
-    public float applyHealth(Boat boatAffected){
+    /**
+     * Apply the health effect.
+     *
+     * @param boatAffected The boat being affected by the power up.
+     * @return A float of the delta between the health before and after the power up.
+     */
+    public float applyHealth(Boat boatAffected) {
         float healthBefore = boatAffected.currentHealth;
         boatAffected.setCurrentHealth(Math.min(boatAffected.getCurrentHealth() + type.effect, boatAffected.boatType.maxHealth));
         return boatAffected.currentHealth - healthBefore;
     }
 
-    public float applyAgility(Boat boatAffected){
+    /**
+     * Apply the agility effect.
+     *
+     * @param boatAffected The boat being affected by the power up.
+     * @return A float of the delta between the agility before and after the power up.
+     */
+    public float applyAgility(Boat boatAffected) {
         float handlingBefore = boatAffected.getCurrentHandling();
         boatAffected.increaseHandling(type.effect);
         return boatAffected.getCurrentHandling() - handlingBefore;
     }
 
-    public float applyStamina(Boat boatAffected){
+    /**
+     * Apply the stamina effect.
+     *
+     * @param boatAffected The boat being affected by the power up.
+     * @return A float of the delta between the agility before and after the power up.
+     */
+    public float applyStamina(Boat boatAffected) {
         float staminaBefore = boatAffected.currentStamina;
         boatAffected.setCurrentStamina(Math.min(boatAffected.getCurrentStamina() + type.effect, boatAffected.maxStamina));
         return boatAffected.currentStamina - staminaBefore;
     }
 
-    public long applyTimer(Boat boatAffected){
+    /**
+     * Apply the timer effect.
+     *
+     * @param boatAffected The boat being affected by the power up.
+     * @return A long of the delta between the total time before and after the power up.
+     */
+    public long applyTimer(Boat boatAffected) {
         long timeBefore = boatAffected.totalTime;
         boatAffected.setTotalTime(boatAffected.getTotalTimeLong() - (long) type.effect);
         return boatAffected.totalTime - timeBefore;
     }
 
-    //TODO: Run this when the player loads the game
+    /**
+     * Apply the no collide effect.
+     *
+     * @param timedBoat The boat being timed to removed the effect after the desired time.
+     * @return A boolean of the status of the no collide of the boat affected.
+     */
     private boolean applyCollide(final Boat timedBoat) {
+        //TODO: Run this when the player loads the game
         timedBoat.setNoCollide(true);
         Timer.Task countDown = new Timer.Task() {
             @Override
@@ -120,10 +196,15 @@ public class PowerUp extends Entity {
     }
 
     /*
-    * End
+     * End
      */
 
-
+    /**
+     * Compares an object to an power up and determines if the object is an identical power up.
+     *
+     * @param obj The object being checked against.
+     * @return A boolean of weather the object is an identical power up.
+     */
     @Override
     public boolean equals(Object obj) {
         if (obj == null) {
@@ -138,11 +219,22 @@ public class PowerUp extends Entity {
         }
     }
 
+    /**
+     * Move the power up using the given movement characteristic
+     *
+     * @param deltaTime The time since the previous frame.
+     */
     public void move(float deltaTime) {
         this.type.getMover().updateVel(deltaTime, this.constantVel);
         this.vel = constantVel.cpy();
     }
 
+    /**
+     * Add the power up texture to the given SpriteBatch and render it.
+     *
+     * @param batch  The SpriteBatch to be added to.
+     * @param relPos The position to be rendered relative to.
+     */
     public void render(SpriteBatch batch, Vector2 relPos) {
         batch.begin();
         batch.draw(this.image,
@@ -151,6 +243,11 @@ public class PowerUp extends Entity {
         batch.end();
     }
 
+    /**
+     * Used to invert the x-velocity of the power up when it is at an edge.
+     *
+     * @param deltaTime The time since the previous frame.
+     */
     public void bounceEdge(float deltaTime) {
         //this.vel.x = this.constantVel.x * -1;
         //this.pos.x += (this.vel.x * - 1 + 5) * deltaTime;
@@ -158,18 +255,36 @@ public class PowerUp extends Entity {
         this.vel.x = 0;
     }
 
+    /**
+     * Get the type of the power up.
+     *
+     * @return The power up type.
+     */
     public PowerUpType getType() {
         return this.type;
     }
 
+    /**
+     * Get the constant velocity attribute of the power up.
+     *
+     * @return A vector2d of the constant velocity.
+     */
     public Vector2 getConstantVel() {
         return this.constantVel;
     }
 
+    /**
+     * Sets the velocity of the power up.
+     *
+     * @param newVel The new velocity to set the power up to.
+     */
     public void setVel(Vector2 newVel) {
-        this.vel = newVel;
+        this.vel = newVel.cpy();
     }
 
+    /**
+     * Dispose the power up texture.
+     */
     @Override
     public void dispose() {
         this.image.dispose();
