88ec975b2afc925fc5226ea8edddc72df1e2a91a
Fluxticks <benjigarment.appdev@gmail.com>
2021-02-05 16:24:49 +0000

    MovementCharacteristics javadoc done

--- core/src/com/dragonboatrace/game/MovementCharacteristics.java
+++ core/src/com/dragonboatrace/game/MovementCharacteristics.java
@@ -4,32 +4,47 @@ import com.badlogic.gdx.math.Vector2;
 
 import java.util.concurrent.ThreadLocalRandom;
 
+/**
+ * Defines how an obstacle can move.
+ *
+ * @author Jacob Turner
+ */
 public enum MovementCharacteristics {
-    STATIC("static", new Tuple<>(0, 0), new Tuple<>((float) 0, (float) 0)),
-    CONSTANT("constant", new Tuple<>(0, 0), new Tuple<>((float) 0, (float) 0)),
-    WANDER("wander", new Tuple<>(-45, 45), new Tuple<>((float) 0.5, (float) 2));
+    // Angle delta, Wander Delay Range
+    STATIC(new Tuple<>(0, 0), new Tuple<>((float) 0, (float) 0)),
+    CONSTANT(new Tuple<>(0, 0), new Tuple<>((float) 0, (float) 0)),
+    WANDER(new Tuple<>(-45, 45), new Tuple<>((float) 0.5, (float) 2));
 
-    String ID;
+    /**
+     * The angle range in which the obstacle can turn its velocity.
+     */
     Tuple<Integer, Integer> angleDelta;
+    /**
+     * The range of amount of time a obstacle can wait before changing its direction.
+     */
     Tuple<Float, Float> wanderDelayRange;
+    /**
+     * The time since last change.
+     */
     float wanderDelay;
 
-    MovementCharacteristics(String ID, Tuple<Integer, Integer> angleDelta, Tuple<Float, Float> wanderDelayRange) {
-        this.ID = ID;
+    MovementCharacteristics(Tuple<Integer, Integer> angleDelta, Tuple<Float, Float> wanderDelayRange) {
         this.angleDelta = angleDelta;
         this.wanderDelayRange = wanderDelayRange;
         this.wanderDelay = 0;
     }
 
+    /**
+     * Perform the change in velocity given the angle and delay.
+     *
+     * @param deltaTime The time since the previous frame.
+     * @param vel       The velocity to modify.
+     */
     public void updateVel(float deltaTime, Vector2 vel) {
         this.wanderDelay -= deltaTime;
         if (this.wanderDelay <= 0) {
             this.wanderDelay = ThreadLocalRandom.current().nextFloat() * (wanderDelayRange.b - wanderDelayRange.a) + wanderDelayRange.a;
             vel.rotate(ThreadLocalRandom.current().nextInt(angleDelta.a, angleDelta.b + 1));
         }
-    }
-
-    public String getID() {
-        return this.ID;
     }
 }
