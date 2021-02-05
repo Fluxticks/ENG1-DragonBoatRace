6c2480a882644eedfccd46fc188a4c27558745df
Fluxticks <benjigarment.appdev@gmail.com>
2021-02-05 16:29:26 +0000

    Tuple javadoc done

--- core/src/com/dragonboatrace/game/Tuple.java
+++ core/src/com/dragonboatrace/game/Tuple.java
@@ -1,14 +1,39 @@
 package com.dragonboatrace.game;
 
+/**
+ * A generic type tuple used to hold a pair of values.
+ *
+ * @param <A> Generic type that defines the type of the first value.
+ * @param <B> Generic type that defines the type of the second value.
+ * @author Jacob Turner
+ */
 public class Tuple<A, B> {
+    /**
+     * The first value in the tuple. Of type A
+     */
     public A a;
+    /**
+     * The second value in the tuple. Of type B
+     */
     public B b;
 
+    /**
+     * Creates a tuple with values a and b, of types A and B respectively.
+     *
+     * @param a The first value in the tuple.
+     * @param b The second value in the tuple.
+     */
     public Tuple(A a, B b) {
         this.a = a;
         this.b = b;
     }
 
+    /**
+     * Assert if a given Object is identical to a given tuple.
+     *
+     * @param obj The object being compared to the tuple.
+     * @return A boolean of if the given object is identical to the tuple.
+     */
     @Override
     public boolean equals(Object obj) {
         if (obj.getClass() == Tuple.class) {
@@ -19,6 +44,11 @@ public class Tuple<A, B> {
         }
     }
 
+    /**
+     * Convert the tuple to a string.
+     *
+     * @return A string representing the tuple.
+     */
     public String toString() {
         return String.format("Tuple<%s, %s>", this.a.toString(), this.b.toString());
     }
