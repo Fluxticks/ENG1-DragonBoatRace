dd8b2d7ee819584900206baba559ddd10e6d77bb
Fluxticks <benjigarment.appdev@gmail.com>
2021-02-05 13:06:27 +0000

    Added testing of applying power up effects

--- tests/src/com/dragonboatrace/game/PowerUpTest.java
+++ tests/src/com/dragonboatrace/game/PowerUpTest.java
@@ -1,24 +1,10 @@
-/*******************************************************************************
- * Copyright 2015 See AUTHORS file.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- ******************************************************************************/
-
 package com.dragonboatrace.game;
 
 import static org.junit.Assert.assertTrue;
 
 import com.badlogic.gdx.math.Vector2;
+import com.dragonboatrace.game.entities.BoatType;
+import com.dragonboatrace.game.entities.PlayerBoat;
 import com.dragonboatrace.game.entities.PowerUp;
 import com.dragonboatrace.game.entities.PowerUpType;
 import org.junit.Assert;
@@ -47,5 +33,66 @@ public class PowerUpTest {
         testingPowerup.setVel(new Vector2(10, 10));
         Assert.assertEquals(testingPowerup.getConstantVel(), new Vector2(0, 0));
 
+    }
+
+    @Test
+    public void applySpeedTest(){
+        PlayerBoat boat = new PlayerBoat(BoatType.TESTING, new Vector2(), new Tuple<Float, Float>(0f, 500f));
+        PowerUp speedPower = new PowerUp(PowerUpType.SPEED, new Vector2(), new Vector2());
+        float expectedDelta = speedPower.getType().getEffect();
+        float outputDelta = speedPower.applySpeed(boat);
+        Assert.assertEquals(expectedDelta, outputDelta, 0.0);
+    }
+
+    @Test
+    public void applyHealthTest(){
+        PlayerBoat boat = new PlayerBoat(BoatType.TESTING, new Vector2(), new Tuple<Float, Float>(0f, 500f));
+        PowerUp healthPower = new PowerUp(PowerUpType.HEALTH, new Vector2(), new Vector2());
+        float expectedDelta = healthPower.getType().getEffect();
+        float outputDelta = healthPower.applySpeed(boat);
+        Assert.assertEquals(expectedDelta, outputDelta, 0.0);
+    }
+
+    @Test
+    public void applyAgilityTest(){
+        PlayerBoat boat = new PlayerBoat(BoatType.TESTING, new Vector2(), new Tuple<Float, Float>(0f, 500f));
+        PowerUp agilityPower = new PowerUp(PowerUpType.AGILITY, new Vector2(), new Vector2());
+        float expectedDelta = agilityPower.getType().getEffect();
+        float outputDelta = agilityPower.applySpeed(boat);
+        Assert.assertEquals(expectedDelta, outputDelta, 0.0);
+    }
+
+    @Test
+    public void applyStaminaTest(){
+        PlayerBoat boat = new PlayerBoat(BoatType.TESTING, new Vector2(), new Tuple<Float, Float>(0f, 500f));
+        PowerUp staminaPower = new PowerUp(PowerUpType.STAMINA, new Vector2(), new Vector2());
+        float expectedDelta = staminaPower.getType().getEffect();
+        float outputDelta = staminaPower.applySpeed(boat);
+        Assert.assertEquals(expectedDelta, outputDelta, 0.0);
+    }
+
+    @Test
+    public void applyTimerTest(){
+        PlayerBoat boat = new PlayerBoat(BoatType.TESTING, new Vector2(), new Tuple<Float, Float>(0f, 500f));
+        PowerUp timerPower = new PowerUp(PowerUpType.TIMER, new Vector2(), new Vector2());
+        float expectedDelta = timerPower.getType().getEffect();
+        float outputDelta = timerPower.applySpeed(boat);
+        Assert.assertEquals(expectedDelta, outputDelta, 0.0);
+    }
+
+    @Test
+    public void applyNoCollideTest(){
+        PlayerBoat boat = new PlayerBoat(BoatType.TESTING, new Vector2(), new Tuple<Float, Float>(0f, 500f));
+        PowerUp noCollidePower = new PowerUp(PowerUpType.NOCOLLIDE, new Vector2(), new Vector2());
+        float expectedDelta = noCollidePower.getType().getEffect();
+        float outputDelta = noCollidePower.applySpeed(boat);
+        Assert.assertEquals(expectedDelta, outputDelta, 0.0);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void invalidPowerType(){
+        PlayerBoat boat = new PlayerBoat(BoatType.TESTING, new Vector2(), new Tuple<Float, Float>(0f, 500f));
+        PowerUp invalidPower = new PowerUp(PowerUpType.TESTING, new Vector2(), new Vector2());
+        invalidPower.applyEffect(boat);
     }
 }
