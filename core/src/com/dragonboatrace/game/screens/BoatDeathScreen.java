bc324e51288b83df2184546e1f4fca10eb223b37
Fluxticks <benjigarment.appdev@gmail.com>
2021-02-07 11:28:51 +0000

    BoatDeathScreen javadoc done

--- core/src/com/dragonboatrace/game/screens/BoatDeathScreen.java
+++ core/src/com/dragonboatrace/game/screens/BoatDeathScreen.java
@@ -7,14 +7,30 @@ import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.Texture;
 import com.dragonboatrace.game.DragonBoatRace;
 
+/**
+ * The screen that shows when the player boat has run out of health.
+ *
+ * @author Jacob Turner
+ */
 public class BoatDeathScreen extends ScreenAdapter {
 
+    /**
+     * The instance of the DragonBoatRace.
+     */
     DragonBoatRace game;
 
+    /**
+     * Creates a new screen to display.
+     *
+     * @param game The instance of the game.
+     */
     public BoatDeathScreen(DragonBoatRace game) {
         this.game = game;
     }
 
+    /**
+     * Display the screen.
+     */
     @Override
     public void show() {
         Gdx.input.setInputProcessor(new InputAdapter() {
@@ -28,9 +44,13 @@ public class BoatDeathScreen extends ScreenAdapter {
         });
     }
 
+    /**
+     * Render the text to tell the user that the game is over.
+     *
+     * @param deltaTime The time since the previous frame.
+     */
     @Override
-    public void render(float delta) {
-
+    public void render(float deltaTime) {
         game.batch.begin();
         game.batch.draw(new Texture("menus/boat broke.png"), 0, 0);
         game.font.draw(game.batch, "Press SPACE to restart :(", Gdx.graphics.getWidth() * .1f, Gdx.graphics.getHeight() * .5f);
